59daf30d0a01d02b7b4b9218af25485852a8fe63
chankxow <101338054+chankxow@users.noreply.github.com>
2023-06-24 22:01:46 +0700

    Create es4.js

---
+++ es4.js
@@ -0,0 +1,10 @@
+//ทำงานร่วมกับSTRING 
+
+let Name = "Chankxow"
+let Address = "77 หมู่ 4 ซอย 7 "
+let City = "อุดรธานี"
+
+const address = `สวัสดีฉันชื่อ ${Name} อยู่บ้านเลขที่ ${Address}
+จังหวัด ${City}`
+
+console.log(address)
