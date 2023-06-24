4fbc9d2912c2a8197230d94759fb90b6dd8d6b19
chankxow <101338054+chankxow@users.noreply.github.com>
2023-06-24 19:39:59 +0700

    prettier

---
+++ es3.js
@@ -0,0 +1,13 @@
+//Object
+
+//ตัวแปรเก็บข้อมูลลูกค้า
+const username = "Chnakxow"
+const age = 25
+const address= "Udonthani"
+
+const Customer = {
+    UserName : username,
+    Age : age,
+    Address : address
+}
+console.log(Customer)
