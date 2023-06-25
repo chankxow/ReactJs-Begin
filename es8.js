be7a7c2b5d0f9907b9e7e90d0358cfc2b0daeee0
chankxow <101338054+chankxow@users.noreply.github.com>
2023-06-25 22:12:08 +0700

    Create es8.js

---
+++ es8.js
@@ -0,0 +1,11 @@
+//default Parameter
+
+getDataCustomer = (customerName,customerAddress='Bangkok')=>{
+
+    const address = `yourNam : ${customerName}
+    yourAddress : ${customerAddress}`
+    return address
+}
+
+console.log('John','Udon')
+console.log('Jojo')
