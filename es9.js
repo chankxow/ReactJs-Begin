b5e865850fd648e36ce92339caea9f8b46b35779
chankxow <101338054+chankxow@users.noreply.github.com>
2023-06-25 22:48:50 +0700

    Create es9.js

---
+++ es9.js
@@ -0,0 +1,12 @@
+//join,concat
+
+const dataX = [200,300,400]
+const dataY = [200,300,400]
+
+console.log(dataX)
+const result = dataX.join("/")
+console.log(result)
+
+const conData = dataX.concat(dataY)
+
+console.log(conData)
