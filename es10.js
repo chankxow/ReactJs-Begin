c3c6bb3405953a6a6df04e27d9721ed4a4a1cc31
chankxow <101338054+chankxow@users.noreply.github.com>
2023-06-26 22:09:45 +0700

    Update es10.js

--- es10.js
+++ es10.js
@@ -8,8 +8,18 @@ data.push(650)
 data.push(...[2005,6005]) //ถ้าไม่ใส่Spread operator จะเป็นสมาชิกอาเรย์
 console.log(data)
 
-//pop ดึงข้อมูลตัวสุดท้ายออก
+//pop ดึงข้อมูลอาเรย์ตัวสุดท้ายออก
 
 data.pop()
 data.pop()
+console.log(data)
+
+//shift ดึงข้อมูลอาเรย์ตัวด้านหน้า
+
+data.shift()
+console.log(data)
+
+//unshift กำหนดตัวใหม่ขึ้นข้างหน้าในอาเรย์
+
+data.unshift(5462)
 console.log(data)
