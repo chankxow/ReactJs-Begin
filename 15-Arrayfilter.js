ea223c83e50b0556cd8e1514ca7df52df6843fb5
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-07 21:54:04 +0700

    Update 15-Arrayfilter.js

--- 15-Arrayfilter.js
+++ 15-Arrayfilter.js
@@ -8,5 +8,18 @@ const result = data.filter(element =>{
 
 console.log(result)
 
+const dataMember = [
+    {name:"Bang",salary:26000,department:"DevOps"},
+    {name:"Senith",salary:25800,department:"Tester"},
+    {name:"Ruser",salary:35600,department:"Design"},
+    {name:"Ester",salary:48500,department:"ProjectManager"}
+]
+
+const resultDataMember = dataMember.filter(element=>{
+    return element.salary>30000
+})
+
+console.log(resultDataMember)
+
 
 
