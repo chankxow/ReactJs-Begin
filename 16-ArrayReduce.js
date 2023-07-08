8e67b48c0c2e10a4969d82f746030fe669c40ab4
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-08 22:55:44 +0700

    Update 16-ArrayReduce.js

--- 16-ArrayReduce.js
+++ 16-ArrayReduce.js
@@ -3,8 +3,23 @@
 
 
 const data = [10,20,30,40,50,60]
-// array.reduce((ค่าที่ถูฏประมวลผล,element)=>{},ค่าเริ่มต้น)
+// array.reduce((ค่าที่ถูกประมวลผล,element)=>{},ค่าเริ่มต้น)
+// value = 0
 
-const dataReduce = data.reduce(()=>{
-
+const dataReduce = data.reduce((value,element)=>{
+    const total = element+value
+    return total
 },0) 
+
+console.log(`ผลรวมข้อมูลคือ ${dataReduce}`)
+
+//cart
+
+const cart = [
+    {name:"gun",price:25000},
+    {name:"knife",price:2500},
+]
+
+const cartReduce = cart.reduce((value,element)=>element.price+value,0)
+
+console.log(`ผลรวมสินค้าทั้งหมดราคา ${cartReduce}`)
