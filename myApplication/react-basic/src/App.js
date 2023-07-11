deb1f7510539d9ff5ec4a16ed0b94fbcd075215c
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-11 22:35:04 +0700

    Update App.js

--- myApplication/react-basic/src/App.js
+++ myApplication/react-basic/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
+const Title = ()=><h1>บัญชีรายรับราย - รายจ่าย</h1>
+const Description =()=><p>บันทึกข้อมูลบัญชีรายรับ-รายจ่าย</p>
+const Transaction =()=>{
+  return(
+  <ul>
+    <li>ค่าเดินทาง<span>-200</span></li>
+    <li>ค่าเดินทาง<span>+20000</span></li>
+    <li>ค่าเดินทาง<span>-500</span></li>
+  </ul>
+  );
+  
+}
 
 function App() {
   return (
     <>
-    <article>
-    <h1>บัญชีรายรับราย - รายจ่าย</h1>
-    <p>บันทึกข้อมูลบัญชีรายรับ-รายจ่าย</p>
-    <ul>
-      <li>ค่าเดินทาง<span>-200</span></li>
-      <li>ค่าเดินทาง<span>+20000</span></li>
-      <li>ค่าเดินทาง<span>-500</span></li>
-    </ul>
-    </article>
-    
-   
+    <Title/>
+    <Description/>
+    <Transaction/>
     </>
   );
 
