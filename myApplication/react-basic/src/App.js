b6b8fa53d50eae0d94538cd96e517a5386de0a59
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-11 22:03:48 +0700

    Update App.js

--- myApplication/react-basic/src/App.js
+++ myApplication/react-basic/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>บัญชีรายรับรายจ่าย</h1>
+    <>
+    <article>
+    <h1>บัญชีรายรับราย - รายจ่าย</h1>
+    <p>บันทึกข้อมูลบัญชีรายรับ-รายจ่าย</p>
+    <ul>
+      <li>ค่าเดินทาง<span>-200</span></li>
+      <li>ค่าเดินทาง<span>+20000</span></li>
+      <li>ค่าเดินทาง<span>-500</span></li>
+    </ul>
+    </article>
+    
+   
+    </>
   );
 
 
