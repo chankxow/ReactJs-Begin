7b5e9ab8c56545d4099fa228df28f5565346cec7
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-11 21:52:41 +0700

    รี

--- myApplication/react-basic/src/index.js
+++ myApplication/react-basic/src/index.js
@@ -13,7 +13,7 @@ function HelloComponent(){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloComponent/>
+    <App/>
   </React.StrictMode>
 );
 
