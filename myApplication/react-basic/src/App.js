81d4b8efe51fe5f54a35e9f8bac566bf8a764270
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-12 21:24:53 +0700

    transacstion

--- myApplication/react-basic/src/App.js
+++ myApplication/react-basic/src/App.js
@@ -1,21 +1,11 @@
+import Transaction from './component/transaction'
+
 const Title = ()=><h1>บัญชีรายรับราย - รายจ่าย</h1>
-const Description =()=><p>บันทึกข้อมูลบัญชีรายรับ-รายจ่าย</p>
-const Transaction =()=>{
-  return(
-  <ul>
-    <li>ค่าเดินทาง<span>-200</span></li>
-    <li>ค่าเดินทาง<span>+20000</span></li>
-    <li>ค่าเดินทาง<span>-500</span></li>
-  </ul>
-  );
-  
-}
 
 function App() {
   return (
     <>
     <Title/>
-    <Description/>
     <Transaction/>
     </>
   );
---
+++ myApplication/react-basic/src/component/item.js
@@ -0,0 +1,7 @@
+const Item = ()=>{
+    const name = "โรงแรม"
+    const price = 2500
+    return <li>{name}<span>- {price}</span></li>
+}
+
+export default Item
