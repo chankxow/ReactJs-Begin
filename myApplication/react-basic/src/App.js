f983aeaabdd500d38505fe64cf88582114bb5046
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-12 21:53:14 +0700

    z

--- myApplication/react-basic/src/App.js
+++ myApplication/react-basic/src/App.js
@@ -1,13 +1,11 @@
 import Transaction from './component/transaction'
 
-const design = {color:'red',textAlign:'center',fontSize:'1.5em'}
 
-const Title = ()=><h1 style={design}>บัญชีรายรับราย - รายจ่าย</h1>
 
 function App() {
   return (
     <>
-    <Title/>
+    <h1 className='container'>บัญชีรายรับราย - รายจ่าย</h1>
     <Transaction/>
     </>
   );
