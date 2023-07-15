695f98c516a63bfe09ca10f550c0b03ad6a29c3c
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-15 20:47:29 +0700

    Update transaction.js

--- myApplication/react-basic/src/component/transaction.js
+++ myApplication/react-basic/src/component/transaction.js
@@ -5,6 +5,9 @@ const Transaction =()=>{
     return(
     <ul className="item-list">
       <Item title='ค่าอาหาร' amount='250'/>
+      <Item title='ค่าไฟ' amount='150'/>
+      <Item title='ค่าขนม' amount='500'/>
+      <Item title='ค่าน้ำมัน' amount='400'/>
     </ul>
     );
 }
