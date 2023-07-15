f0a94c5e93294f41beb8e707387aa213504aff45
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-15 23:15:59 +0700

    Update transaction.js

--- myApplication/react-basic/src/component/transaction.js
+++ myApplication/react-basic/src/component/transaction.js
@@ -5,15 +5,15 @@ const Transaction =()=>{
     {title:'ค่าอาหาร',amount:'250'},
     {title:'ค่าน้ำ',amount:'150'},
     {title:'ค่าขนม',amount:'500'},
-    {title:'ค่าน้ำมัน',amount:'400'}
+    {title:'ค่าน้ำมัน',amount:'400'},
+    {title:'ค่าที่พัก',amount:'1800'}
   ]
     return(
     <ul className="item-list">
-      <Item title={data[0].title} amount={data[0].amount}/>
-      <Item title={data[1].title} amount={data[1].amount}/>
-      <Item title={data[2].title} amount={data[2].amount}/>
-      <Item title={data[3].title} amount={data[3].amount}/>
-    </ul>
+      {data.map((element)=>{
+        return <Item title={element.title} amount={element.amount}/>
+      })}
+      </ul>
     );
 }
 
