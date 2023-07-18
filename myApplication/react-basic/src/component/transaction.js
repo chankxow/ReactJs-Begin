837697a0c7472a7dc9446548ae8b5ce87dc8cfb7
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-18 18:47:21 +0700

    uuidimport

--- myApplication/react-basic/src/component/transaction.js
+++ myApplication/react-basic/src/component/transaction.js
@@ -1,19 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
 import Item from "./item";
 import './transaction.css'
+
+
+
 const Transaction =()=>{
   const data = [
-    {id:1,title:'ค่าอาหาร',amount:'250'},
-    {id:2,title:'ค่าน้ำ',amount:'150'},
-    {id:3,title:'ค่าขนม',amount:'500'},
-    {id:4,title:'ค่าน้ำมัน',amount:'400'},
-    {id:5,title:'ค่าที่พัก',amount:'1800'},
-    {id:6,title:'ค่างวดรถ',amount:'2300'}
+    {title:'ค่าอาหาร',amount:'250'},
+    {title:'ค่าน้ำ',amount:'150'},
+    {title:'ค่าขนม',amount:'500'},
+    {title:'ค่าน้ำมัน',amount:'400'},
+    {title:'ค่าที่พัก',amount:'1800'},
+    {title:'ค่างวดรถ',amount:'2300'}
 
   ]
     return(
     <ul className="item-list">
       {data.map((element)=>{
-        return <Item key={element.id}{...element}/>
+        return <Item key={uuidv4()} {...element}/>
       })}
       </ul>
     );
